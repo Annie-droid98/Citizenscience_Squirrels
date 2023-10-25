ea7d10ed053a05fa12065b6fcc4b197c13f2af7d
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2023-10-25 16:09:40 +0200

    Counting streamlined and finished

--- R/2b_CountGbif.R
+++ R/2b_CountGbif.R
@@ -29,63 +29,55 @@ Mammalia_GB <- vroom::vroom(temp, quote="",show_col_types = FALSE) %>%
     as.data.frame %>%
     filter((! is.na(decimalLatitude))) %>%
     sf::st_as_sf(coords = c(2,3)) %>%
+    ## set the correct coordinate system
     st_set_crs(4326) %>%
+    ## and transform it for uniformity with the grid and landuse data
     st_transform(st_crs(3035))  %>% 
-    dplyr::mutate(lon = sf::st_coordinates(.)[,"X"],
-                  lat = sf::st_coordinates(.)[,"Y"])
+    mutate(lon = sf::st_coordinates(.)[,"X"],
+           lat = sf::st_coordinates(.)[,"Y"])
 
 ## Publisher categorisation
 Publishers <- vroom::vroom("input_data/SquirrelPublisherBelow1000obs.csv",
                            show_col_types = FALSE)
 
-### We don't non-categorized publishers here they'll be NA (mege with
-### all=TRUE)
+### Non-categorized publishers are here NA (mege with all=TRUE)
 Mammalia_GB <- merge(Mammalia_GB, Publishers,
-                     by="datasetKey", all=TRUE)
+                     by="datasetKey", all=TRUE) %>%
+    ## keep only records with species
+    filter(!is.na(species))
+
+### merge the grids and the counts to allow counting
+## Mammalia_GB_count_10km <- Grid_3035 %>%
 
 Mammalia_GB_count_10km <- Grid_3035 %>%
     st_join(st_sf(Mammalia_GB)) %>%
-    ## First we count Ci 
-    transform(isVulgaris = species%in%"Sciurus vulgaris",
-              isCarolinensis = species%in%"Sciurus carolinensis",
-              isMartes = species%in%"Martes martes")%>%
-    ### se count seperately for each category of observers
-    group_by(year, CELLCODE, Observer) %>%
-    count(isVulgaris, isCarolinensis, isMartes,
-          countMammalia = !isVulgaris&!isCarolinensis&!isMartes)%>%
-    transform(what = ifelse(isVulgaris,"S.vulgaris",
-                     ifelse(isCarolinensis,"S.carolinensis",
-                     ifelse(isMartes,"M.martes", "countMammalia"))))%>%
-    tidyr::spread(what,n)
+    ## again drop grids with no species counts
+    filter(!is.na(species))  %>%
+    group_by(year, CELLCODE, Observer, FocusTaxaTorF, .drop=FALSE) %>%
+    ## do the actual counting 
+    summarize(CountT_mammalia = n(),
+              CountT_vulgaris = sum(species%in%"Sciurus vulgaris"),
+              CountT_carolinensis = sum(species%in%"Sciurus carolinensis"),
+              CountT_marten = sum(species%in%"Martes martes")) %>%
+    mutate(PropT_carolinensis = CountT_carolinensis/CountT_mammalia, 
+           PropT_vulgaris = CountT_vulgaris/CountT_mammalia, 
+           PropT_marten = CountT_marten/CountT_mammalia)
 
-## middlepoint of each grid cell as coordinate
-Mammalia_GB_count_10km$Centergrid <-st_centroid(Mammalia_GB_count_10km$geometry)
-Mammalia_GB_count_10km <- Mammalia_GB_count_10km %>%
-    dplyr::mutate(lonC = sf::st_coordinates(Mammalia_GB_count_10km$Centergrid)[,1],
-                  latC = sf::st_coordinates(Mammalia_GB_count_10km$Centergrid)[,2])
+### Not doing the centergrids for now
+## ## middlepoint of each grid cell as coordinate
+## Mammalia_GB_count_10km$Centergrid <-st_centroid(Mammalia_GB_count_10km$geometry)
 
-Mammalia_GB_count_10km <- Mammalia_GB_count_10km %>%
-  unite('IDYearObs', CELLCODE:year:Observer, remove = FALSE)
+## ## middlepoint of each grid cell here citizen science count
+## #Mammalia_GB_count_10km[[1]]$Centergrid <-st_centroid(Mammalia_GB_count_10km[[1]]$geometry)
+## Mammalia_GB_count_10km$Centergrid <-st_centroid(Mammalia_GB_count_10km$geometry)
+## #Mammalia_GB_count_10km_2 <-Mammalia_GB_count_10km[[1]]%>%
+## Mammalia_GB_count_10km_2 <-Mammalia_GB_count_10km%>%
+##   # dplyr::mutate(lon = sf::st_coordinates(Mammalia_GB_count_10km[[1]]$Centergrid)[,1],
+##   #              lat = sf::st_coordinates(Mammalia_GB_count_10km[[1]]$Centergrid)[,2])
+##   dplyr::mutate(lon = sf::st_coordinates(Mammalia_GB_count_10km$Centergrid)[,1],
+##                 lat = sf::st_coordinates(Mammalia_GB_count_10km$Centergrid)[,2])
 
-Mammalia_GB_count_10km <- Mammalia_GB_count_10km %>% 
-  arrange(IDYearObs) %>%
-  group_by(IDYearObs) %>% fill(c(everything()), .direction = "downup") %>% 
-  ungroup() %>% 
-  distinct(IDYearObs, .keep_all = T) %>% 
-  filter(!is.na(year)) %>%
-    mutate(
-        across(all_of(c("S.vulgaris", "S.carolinensis",
-                        "M.martes", "countMammalia")), ~replace_na(.x, 0))
-    )
-
-Mammalia_GB_count_10km <- Mammalia_GB_count_10km %>%
-    transform(AllMammalia = countMammalia +
-                  S.vulgaris + M.martes + S.carolinensis) %>%
-    transform(Proportion_carolinensis = S.carolinensis/AllMammalia, 
-              Proportion_vulgaris = S.vulgaris/AllMammalia, 
-              Proportion_marten = M.martes/AllMammalia)
-
-saveRDS(Mammalia_GB_count_10km, "Counts.rds")
+saveRDS(Mammalia_GB_count_10km, "intermediate_data/Counts.rds")
 
 
 
