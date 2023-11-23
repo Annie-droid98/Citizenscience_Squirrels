85331b9d97f81b80a50676349caa09a146044b6a
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2023-11-23 17:36:36 +0100

    First revision prediction script (5_)

--- R/5_predictions.R
+++ R/5_predictions.R
@@ -3,8 +3,8 @@ library(sf)
 library(ggmap)
 
 
-## We need the original count data this is in the repository or you
-## could count again!
+## We need the original count data, this is available in the
+## repository if you don't want to repeat the count!
 reCount <- FALSE
 
 if(reCount) {
@@ -14,11 +14,11 @@ if(reCount) {
 }
 
 
-## We need the models! These are too large for github storage
-## Do you want to rerun them?
+## We need the models! These are too large for github storage Do you
+## want to rerun them?
 reRunModels <- FALSE
 
-## If you want to read them from files, that's where they shoudl be:
+## If you want to read them from files, that's where they should be:
 modelFile <- "intermediate_data/gh_ignore/Citizenscience_modelle.rds"
 modelFileCaro <- "intermediate_data/gh_ignore/Citizensciencemodelle_caro.rds"
 
@@ -29,8 +29,8 @@ if(!reRunModels) {
         if(file.exists(modelFile) &
            file.exists(modelFileCaro)){
             message("Reading models from local files")
-            results <- readRDS(modelFile)
-            results_caro <- readRDS(modelFileCaro)
+            result <- readRDS(modelFile)
+            result_caro <- readRDS(modelFileCaro)
         } else {
             stop("Download the models from Zenodo ",
                  "using this link: https://zenodo.org/records/10141957  ",
@@ -44,26 +44,33 @@ if(!reRunModels) {
 }
 
 Predictiondf <- data.frame(
+    ## predict for variable proportions of invasive squirrels
     PropT_carolinensis = rep(seq(0, 1, 0.1), each = 4)) %>%
+    ## predict for variable proportions of grey urban land use
     mutate(PropL_Grey_urban = rep(c(0.0, 0.3, 0.6, 0.9), times=11),
+           ## balance this variation with agricultural area
            PropL_Agricultural = rep(c(1, 0.7, 0.4, 0.1), times=11),
-           year_from_2000 = 18, 
+           year_from_2000 = 18, ## predict for 2018
+           ## all otehr proportions zero
            PropL_Green_urban = 0.00, 
            PropL_Broadleafed_Forest = 0.00, 
            PropL_Coniferous_Forest = 0.00, 
            PropL_Mixed_Forest = 0.00, 
            PropL_Other_seminatural = 0.00, 
-           PropL_Waterbodies = 0.00, ## error in initial code?
+           PropL_Waterbodies = 0.00, 
            PropT_marten = 0.00, 
+           ## pick a cell in Northern England for the random effects
            lat = 3615,
            lon = 3185,
+           ## and 100 mammalia counted (to have percent output
+           ## basically)
            CountT_mammalia_log = log(100)) %>%
     mutate(Grey_urban_plot = factor(PropL_Grey_urban, 
                                     labels = c("0% Grey urban", "30% Grey urban", 
                                                "60% Grey urban","90% Grey urban")),
            ## We predict using the first element in the results list
            ## of models! This is the main model (the following models
-           ## are for/from lrs testing
+           ## are for/from likelihood ratio testing)
            predictions = as.vector(predict(result[[1]],
                                            newdata=. ,
                                            type = "response")))
@@ -94,40 +101,48 @@ Predictiondf %>%
 ## plotpseudonew +  facet_wrap(. ~ Grey_urban_z2,ncol=2)
 
 ggsave("figures/Predictiondifferentcarolinensisgreyurban.pdf", width=9, height=5)
-dev.off()
 
 
 ### Prediction map
+Predictions_map <- CountALL_10km %>%
+    ## Citizen science and no focus taxon
+    filter(Observer%in%"Citizen"&
+           !FocusTaxaTorF)%>%
+    ## predict for 2018
+    filter(year == 2018)%>%
+    ## 100 observations
+    mutate(CountT_mammalia_log = log(100)) %>%
+    replace(is.na(.), 0) %>%
+    ## We predict using the first element in the results list
+    ## of models! This is the main model (the following models
+    ## are for/from likelihood ratio testing)
+    mutate(predictions_vulgaris =predict(result[[1]],
+                                         newdata = .,
+                                         type = "response"), 
+           predictions_caro=predict(result_caro[[1]],
+                                    newdata= .,
+                                    type = "response")) # %>%
 
-d_2018 <- CountALL_10km %>% filter(Observer%in%"Citizen"&
-                                     !FocusTaxaTorF)%>%
-  filter(year == 2018)%>%
-  mutate(AllMammalia = 4.60517)
-d_2018[is.na(d_2018)] <- 0
+## ## transform the coordinates back to plot the map ??  not doing this
+## ## right now as it likely needs to be changed for plotting anyways.
 
-Predictions_citizenscience <- d_2018 %>%
-  transform(predictions_vulgaris =predict(fit_vulgaris_2_try, newdata= d_2018, type = "response"))
+## ## I played around with it a bit maybe som of it is useful... 
+## mutate(lon10k = lon*1e+05,
+##        lat10k = lat*1e+05)%>%
+##     sf::st_as_sf(coords = c("lon10k","lat10k"))%>%
+##     st_set_crs(3035)%>%
+##     st_transform(4326) %>%
+##     mutate(lonTrans = sf::st_coordinates(.)[,1],
+##            latTrans = sf::st_coordinates(.)[,2])
 
-Predictions_citizenscience <-Predictions_citizensciencelog() %>%
-  transform(predictions_caro=predict(fits_carolinensis, newdata= df_10kmzeros_2, type = "response"))
-
-#transform the coordinates back to plot the map
-Predictions_retransformed  <- Predictions_citizenscience %>%
-  mutate(lon = lon*100000, lat = lat*100000)%>%
-  sf::st_as_sf(coords = c(16,17))%>%
-  st_set_crs(3035)%>%
-  st_transform(4326)
-
-Predictions_retransformed <- Predictions_retransformed %>%
-  dplyr::mutate(long = sf::st_coordinates(Predictions_retransformed)[,1],
-                lat = sf::st_coordinates(Predictions_retransformed)[,2])  
-
-png(filename = "Predictionmap.png",units="in", width=8, height=8, res=300)
+## Handing over to you Alex! We probably should explore this
+## prediction map a bit, in addition to/while plotting it
 
 #ggmap(myMap_B_toner_2) +
 ggplot()+
-  geom_point(data=Predictions_retransformed, aes(x=long, y=lat, color = predictions_vulgaris))+
-  labs(color = "Predicted S.vulgaris observations")
+    geom_point(data=Predictions_map,
+               aes(x=lon, y=lat, color = predictions_vulgaris))+
+    labs(color = "Predicted S.vulgaris observations") +
 # scale_colour_gradientn(
 #  limits  = range(log(Predictions_retransformed$predictions_vulgaris)),
 # colours = colours[c(1, seq_along(colours), length(colours))],
@@ -136,18 +151,20 @@ ggplot()+
 # scale_colour_continuous(trans = "log10", type = "viridis")+
 theme(legend.key.size = unit(1.8, 'cm'),text = element_text(size = 12))
 #scale_fill_gradient(name = "count", trans = 'log10',
-#         breaks = c(0,15,30,45,60,75,90,105,120,135,150), labels = c(0,15,30,45,60,75,90,105,120,135,150))
+                                        #         breaks = c(0,15,30,45,60,75,90,105,120,135,150),
+# labels = c(0,15,30,45,60,75,90,105,120,135,150))
 
-dev.off()
+ggsave(filename = "figures/Predictionmap.png", units="in", width=8, height=8)
 
 
-png(filename = "Predictionmap_caro.png",units="in", width=8, height=8, res=300)
 
 #ggmap(myMap_B_toner_2) +
 ggplot()+
-  geom_point(data=Predictions_retransformed, aes(x=long, y=lat, color = predictions_carolinensis))+
+    geom_point(data=Predictions_map, aes(x=lon, y=lat,
+                                         color = predictions_caro))+
   labs(color = "Predicted S.carolinensis observations")+
   # scale_colour_continuous(trans = "log10", type = "viridis")+
   theme(legend.key.size = unit(1.8, 'cm'),text = element_text(size = 12))
 
-dev.off()
+ggsave(filename = "figures/Predictionmap_caro.png", units="in", width=8, height=8)
+
