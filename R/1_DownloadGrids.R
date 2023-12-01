3228f4bc864ee1f1bfb635e50aa148c59fba8766
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2023-12-01 18:34:25 +0100

    Use sf rather than raster + rgdal in file 1

--- R/1_DownloadGrids.R
+++ R/1_DownloadGrids.R
@@ -1,8 +1,5 @@
 library(dplyr)
 library(sf)
-library(rgdal)
-library(raster)
-
 
 ## We need to download Ireland and great Britain shape files and then
 ## unite them ;-) 
@@ -12,24 +9,18 @@ download.file("https://www.eea.europa.eu/data-and-maps/data/eea-reference-grids-
               destfile = file.path(tempdir(), "Ireland_shapefile"), mode = "wb")
 unzip(zipfile = file.path(tempdir(), "Ireland_shapefile"), exdir = tempdir())  
 
-Ireland_10grid <- shapefile(file.path(tempdir(), "ie_10km.shp"))
+Ireland_10grid <- read_sf(file.path(tempdir(), "ie_10km.shp"))
 
 #Great Britain
 download.file("https://www.eea.europa.eu/data-and-maps/data/eea-reference-grids-2/gis-files/great-britain-shapefile/at_download/file",
               destfile = file.path(tempdir(), "Great_Britain_shapefile"), mode = "wb")
 unzip(zipfile = file.path(tempdir(), "Great_Britain_shapefile"), exdir = tempdir())  
 
-GB_10grid <- shapefile(file.path(tempdir(), "gb_10km.shp"))
+GB_10grid <- read_sf(file.path(tempdir(), "gb_10km.shp"))
 
-GB_and_IE_grid_10km <- bind(Ireland_10grid, GB_10grid ) |>
-    st_as_sf()
+#Combine Ireland and Great Britain
+GB_and_IE_grid_10km <- rbind(Ireland_10grid, GB_10grid)
 
-Britain10grid <- GB_and_IE_grid_10km |>
-    distinct()
+Britain10grid <- distinct(GB_and_IE_grid_10km)
 
-saveRDS(Britain10grid, "intermediate_data/10kmgrids.rds")
-
-## #as shapefile
-## GB_and_IE_grid_10km_shp <- bind(Ireland_10grid, GB_10grid)
-## saveRDS(GB_and_IE_grid_10km_shp,"intermediate_data/Shapefile_British_Islands.rds")
-
+st_write(Britain10grid, "intermediate_data/10kmgrids.shp", append = FALSE)
